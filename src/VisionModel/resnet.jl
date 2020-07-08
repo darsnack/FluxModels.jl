68fef3a04519e2d8965b74f937bf3f8eaf9c084a
Nabanita Dash <dashnabanita@gmail.com>
2020-07-08 22:32:08 +0530

    final changes

--- src/VisionModel/resnet.jl
+++ src/VisionModel/resnet.jl
@@ -1,51 +1,61 @@
 using Flux
 
-basicblock(inplanes::Int, outplanes::Int, downsample::Bool = false) = downsample ? 
-  Chain(Conv((3, 3), inplanes => outplanes, stride = 1, pad = 1),
-        BatchNorm(outplanes, λ = relu),
-        Conv((3, 3), outplanes => outplanes, stride = 2, pad = 1,
-        BatchNorm(outplanes, λ=relu)) : 
-  Chain(Conv((3, 3), inplanes => outplanes, stride = 1, pad = 1),
-        BatchNorm(outplanes, λ = relu),
-        Conv((3, 3), outplanes => outplanes, stride = 1, pad = 1,
-        BatchNorm(outplanes, λ=relu))
+basicblock(inplanes::Int, outchannels, downsample::Bool = false) = downsample ? 
+  Chain(Conv((3, 3), inplanes => outchannels[1], stride = 2),
+        BatchNorm(inplanes, λ = relu),
+        Conv((3, 3), inplanes => outchannels[2], stride = 1, pad = 1),
+        BatchNorm(outchannels, λ=relu)) : 
+  Chain(Conv((3, 3), inplanes => outchannels[1], stride = 1),
+        BatchNorm(inplanes, λ = relu),
+        Conv((3, 3), inplanes => outchannels[2], stride = 1, pad = 1),
+        BatchNorm(outchannels, λ=relu))
 
-bottleneck(inplanes::Int, outplanes::Int, downsample::Bool = false) = downsample ?
-  Chain(Conv((1, 1), inplanes => inplanes, stride = 1),
+bottleneck(inplanes::Int, outchannels, downsample::Bool = false) = downsample ?
+  Chain(Conv((1, 1), inplanes => outchannels[1], stride = 2),
         BatchNorm(inplanes, λ = relu),
-        Conv((3, 3), inplanes => inplanes, stride = 1, pad = 1),
+        Conv((3, 3), inplanes => outchannels[2], stride = 1, pad = 1),
         BatchNorm(inplanes, λ = relu),
-        Conv((1, 1), inplanes => outplanes, stride = 2),
-        BatchNorm(outplanes, λ = relu)) :
-  Chain(Conv((1, 1), inplanes => inplanes, stride = 1),
+        Conv((1, 1), inplanes => outchannels[3], stride = 1),
+        BatchNorm(outchannels, λ = relu)) :
+  Chain(Conv((1, 1), inplanes => outchannels[1], stride = 1),
         BatchNorm(inplanes, λ = relu),
-        Conv((3, 3), inplanes => inplanes, stride = 1, pad = 1),
+        Conv((3, 3), inplanes => outchannels[2], stride = 1, pad = 1),
         BatchNorm(inplanes, λ = relu),
-        Conv((1, 1), inplanes => outplanes, stride = 1),
-        BatchNorm(outplanes, λ = relu))
+        Conv((1, 1), inplanes => outchannels[3], stride = 1),
+        BatchNorm(outchannels, λ = relu))
 
 projection(inplanes::Int, outplanes::Int, stride::Int) = Chain(Conv((1, 1), inplanes => outplanes, stride=stride),
                                                                BatchNorm((outplanes), λ=relu))
 
 identity(inplanes::Int, outplanes::Int, stride::Int) = +
 
-function resnet(block, shortcut, channel_config, block_config)
+function resnet(block, shortcut_config, channel_config, block_config)
   layers = []
   push!(layers, Conv((7, 7), 3=>inplanes, stride=(2, 2), pad=(3, 3)))
   push!(layers, BatchNorm(inplanes, λ=relu))
   push!(layers, MaxPool((3, 3), stride=(2, 2), pad=(1, 1)))
   inplanes = 64
+  baseplanes = 64
   for nrepeats in block_config
-    for i in 1:nrepeats-1
-      for expansion in channel_config
-        outplanes = inplanes * expansion
-        push!(layers, SkipConnection(block(inplanes, outplanes, i == 1),
-                                     identity(inplanes, outplanes, stride)))
+    outplanes = baseplanes .* channel_config
+    # downsample the first block
+    if shortcut_config == :A
+      # zero padded identity shortcut
+    elseif shortcut_config == :B || shortcut_config == :C
+      push!(layers, SkipConnection(block(inplanes, outplanes, true),
+                                   projection(inplanes, outplanes[end], 2)))
+    end
+    inplanes = outplanes[end]
+    for i in 1:nrepeats
+      if shortcut_config == :A || shortcut_config == :B
+        push!(layers, SkipConnection(block(inplanes, outplanes, false),
+                                     identity(inplanes, outplanes[end], 2)))
+      elseif shortcut_config == :C
+        # projection shortcut
       end
-    end 
-    inplanes *= 2
-    push!(layers, SkipConnection(block(inplanes, outplanes, i == 1),
-                                 projection(inplanes, outplanes, stride)))
+      inplanes = outplanes[end]
+    end
+    baseplanes *= 2
   end
   push!(layers, AdaptiveMeanPool(1, 1))
   push!(layers, x -> flatten(x, 1))
@@ -61,12 +71,12 @@ Dict("resnet18" => ([1, 1], [2, 2, 2, 2]),
      "resnet101" => ([1, 1, 4], [3, 4, 23, 3]),
      "resnet152" => ([1, 1, 4], [3, 8, 36, 3]))
 
-ResNet18() = resnet(basicblock, resnet_config["resnet18"]...)
+ResNet18() = resnet(basicblock, ":A", resnet_config["resnet18"]...)
 
-ResNet34() = resnet(basicblock, resnet_config["resnet34"]...)
+ResNet34() = resnet(basicblock, ":A", resnet_config["resnet34"]...)
 
-ResNet50() = resnet(bottleneck, resnet_config["resnet50"]...)
+ResNet50() = resnet(bottleneck, ":B", resnet_config["resnet50"]...)
 
-ResNet101() = resnet(bottleneck, resnet_config["resnet101"]...)
+ResNet101() = resnet(bottleneck, ":B", resnet_config["resnet101"]...)
 
-ResNet152() = resnet(bottleneck, resnet_config["resnet152"]...)
+ResNet152() = resnet(bottleneck, ":B", resnet_config["resnet152"]...)
