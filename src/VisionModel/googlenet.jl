af19dc2c31a31e98badde760af7ba329291c5cc2
Nabanita Dash <dashnabanita@gmail.com>
2020-07-30 17:16:44 +0530

    googlenet

--- src/VisionModel/googlenet.jl
+++ src/VisionModel/googlenet.jl
@@ -2,35 +2,35 @@ using Flux
 
 export googlenet
 
-function conv_block(inplanes, outplanes, stride, pad, kernel_size)
+function conv_block(inplanes, outplanes, stride, pad, kernelsize)
   conv_layer = []
-  push!(conv_layer, Conv(kernel_size, inplanes => outplanes, stride = stride, pad = pad)
+  push!(conv_layer, Conv(kernelsize, inplanes => outplanes, stride = stride, pad = pad))
   push!(conv_layer, BatchNorm(outplanes, relu))
   return conv_layer
 end
 
 function inception_block(inplanes, out_1x1, red_3x3, out_3x3, red_5x5, out_5x5, pool_proj)
-  branch1 = conv_block(inplanes, out_1x1, kernel_size=(1,1))
+  branch1 = conv_block(inplanes, out_1x1, 0, 0, (1,1))
 
-  branch2 = [conv_block(inplanes, red_3x3, kernel_size=(1,1)),
-             conv_block(red_3x3, out_3x3, pad=1, kernel_size=(3,3))]        
+  branch2 = [conv_block(inplanes, red_3x3, 0, 0, (1,1)),
+             conv_block(red_3x3, out_3x3, 0, 1, (3,3))]        
 
-  branch3 = [conv_block(inplanes, red_5x5, kernel_size=(1,1)),
-             conv_block(red_5x5, out_5x5, pad=2, kernel_size=(5,5))] 
+  branch3 = [conv_block(inplanes, red_5x5, 0, 0, (1,1)),
+             conv_block(red_5x5, out_5x5, 0, 2, (5,5))] 
 
   branch4 = [MaxPool((3, 3), stride=1, pad=1),
-             conv_block(inplanes, pool_proj, kernel_size=(1,1))]
+             conv_block(inplanes, pool_proj, 0, 0, (1,1))]
  
-  inception_layer = cat(branch1, branch2..., branch3..., branch4...; dims=3)
+  inception_layer = cat(branch1, branch2..., branch3..., branch4...; dims=1)
 
   return inception_layer
 end
 
 function googlenet()
-  layers = Chain(conv_block(inplanes=3, outplanes=64, stride=2, pad=3, kernel_size=(7,7)),
+  layers = Chain(conv_block(3, 64, 2, 3, (7,7)),
                  MaxPool((3,3), stride=2, pad=1),
-                 conv_block(inplanes=64, outplanes=64, kernel_size=(1,1)),
-                 conv_block(inplanes=64, outplanes=192, pad=1, kernel_size=(3,3)),
+                 conv_block(64, 64, 0, 0, (1,1)),
+                 conv_block(64, 192, 0, 1, (3,3)),
                  MaxPool((3,3), stride=2, pad=1),
                  inception_block(192, 64, 96, 128, 16, 32, 32),
                  inception_block(256, 128, 128, 192, 32, 96, 64),
