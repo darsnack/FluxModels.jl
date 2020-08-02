bcc06adbbdfce11b889d88e24611d68ee63acec2
Nabanita Dash <dashnabanita@gmail.com>
2020-08-02 19:38:26 +0530

    changed tests for vgg

--- test/VisionModel/vgg.jl
+++ test/VisionModel/vgg.jl
@@ -3,9 +3,9 @@ using FluxModels
 using Test
 
 @testset "VGG" for model in [vgg11, vgg11bn, vgg13, vgg13bn, vgg16, vgg16bn, vgg19, vgg19bn]
-  imsize = rand(Float32, 256, 256, 3, 50)
+  imsize = (224, 224)
   m = model(imsize)  
   
-  @test size(m) == (1000, 50)
+  @test size(m(rand(Float32, imsize..., 3, 50))) == (1000, 50)
 end
 
